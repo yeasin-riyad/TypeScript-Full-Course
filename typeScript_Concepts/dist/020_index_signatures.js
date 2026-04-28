ced7363494c4d1841086a3b0b6fdf371bc483cb6
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-04-28 11:10:05 +0600

    index_signature module complete

--- typeScript_Concepts/dist/020_index_signatures.js
+++ typeScript_Concepts/dist/020_index_signatures.js
@@ -4,6 +4,11 @@ counters["Likes"] = 1;
 counters["Comments"] = 2;
 counters["shares"] = 100;
 const mm = { likes: 1, views: 100, shares: 23 };
+// 👉 এখন শুধু Keys change করলেই সব update
+//  Dynamic Case (Real Project)
+const METRIC_KEYS = ["likes", "views", "shares"];
+// 👉 এটা industry-level pattern 🔥
+// 👉 single source of truth
 const priceMap = new Map();
 priceMap.set("likes", 1);
 const lm = {};
--- typeScript_Concepts/src/020_index_signatures.ts
+++ typeScript_Concepts/src/020_index_signatures.ts
@@ -6,8 +6,25 @@ counters["Likes"] = 1;
 counters["Comments"] = 2;
 counters["shares"] = 100;
 
+type Metrices1={
+    likes:number,
+    views:number,
+    shares:number
+}
+
 type Metrices = Record<"likes" | "views" | "shares", number>; // tight and safer
 const mm: Metrices = { likes: 1, views: 100, shares: 23 };
+
+// Scalability Example (এখানেই Record জিতে যায় 🔥)
+type Keys = "likes" | "views" | "shares";
+type Metrices11 = Record<Keys, number>;
+// 👉 এখন শুধু Keys change করলেই সব update
+
+//  Dynamic Case (Real Project)
+const METRIC_KEYS = ["likes", "views", "shares"] as const;
+type Metrices12 = Record<(typeof METRIC_KEYS)[number], number>;
+// 👉 এটা industry-level pattern 🔥
+// 👉 single source of truth
 
 const priceMap = new Map<string, number>();
 priceMap.set("likes", 1);

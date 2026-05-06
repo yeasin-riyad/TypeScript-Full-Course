74361cd945e9f052be694311ceccb2376611490c
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-05-06 13:11:19 +0600

    027_util_function_helpers.ts modified

--- typeScript_Concepts/src/027_util_function_helpers.ts
+++ typeScript_Concepts/src/027_util_function_helpers.ts
@@ -3,7 +3,12 @@
 //InstanceType<Ctor>
 // ConstructorParameters<C>
 
-function ExtractUserInfo(id: string, isExtraInfo = false) {
+
+// 🧠 1. as keyword কী?
+// TypeScript-এ as মানে type assertion
+// 👉 “আমি compiler-কে বলছি—এই value-টা এই type হিসেবে ধরো”
+// value as Type
+function ExtractUserInfo(id: string, isExtraInfo:boolean = false) {
   return {
     id,
     name: "Sangam",
@@ -11,13 +16,37 @@ function ExtractUserInfo(id: string, isExtraInfo = false) {
   };
 }
 
+//1. ReturnType<F>
+// 👉 এটা কী করে?
+// ExtractUserInfo function যা return করে
+// সেটার type automatically extract করে
 type GetUserReturnInfo = ReturnType<typeof ExtractUserInfo>;
+// 👉 Return type হবে:
+// {
+//   id: string;
+//   name: string;
+//   log: string | undefined;
+// }
+
+// 🔥 কেন useful?
+// manually type লিখতে হয় না
+// function change হলে type auto update হয়
+
+
+//2. Parameters<F>
+// 👉 এটা কী করে?
+// function এর parameters এর type extract করে
 type GetUserParamsInfo = Parameters<typeof ExtractUserInfo>;
+// 👉 এটা return করে:
+// [string, boolean?]
 
 const argsInfo: GetUserParamsInfo = ["u1", true];
-const resultInfo: GetUserReturnInfo = ExtractUserInfo(...argsInfo);
+const argsInfo2: GetUserParamsInfo = ["u2"]; // isExtraInfo optional
+const resultInfo: GetUserReturnInfo = ExtractUserInfo(...argsInfo2);
 
-class PersonN1 {
+console.log(resultInfo);
+
+class PersonN190 {
   constructor(public name: string, public age: number) {}
 
   greet() {
@@ -25,10 +54,21 @@ class PersonN1 {
   }
 }
 
-type PersonInstanceN1 = InstanceType<typeof PersonN1>;
-type PersonCtorArgsN1 = ConstructorParameters<typeof PersonN1>;
+// 3. InstanceType<Ctor> → class instance type
+// 👉 এটা কী করে?
+// class এর instance এর type extract করে
+type PersonInstanceN1 = InstanceType<typeof PersonN190>;
+// 👉 PersonInstanceN1 এর type হবে:
+// {
+//   name: string;
+//   age: number;
+//   greet(): string;
+// }
+type PersonCtorArgsN1 = ConstructorParameters<typeof PersonN190>;
+// 👉 PersonCtorArgsN1 এর type হবে:
+// [string, number]
 
 const resultInfo1: PersonCtorArgsN1 = ["Sangam", 29];
-const abc: PersonInstanceN1 = new PersonN1(...resultInfo1);
+const abc: PersonInstanceN1 = new PersonN190(...resultInfo1);
 
 console.log(abc.greet());
